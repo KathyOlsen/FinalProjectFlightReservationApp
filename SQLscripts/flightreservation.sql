4b4a2124e631d481b89bfd3ec12512dc425f179f
KathyOlsen <katherinekolsen@verizon.net>
2019-10-21 16:02:44 -0400

    initial commit

---
+++ SQLscripts/flightreservation.sql
@@ -0,0 +1,8 @@
+create database flightreservation;
+use flightreservation;
+
+-- For each new program:
+-- (1) copy and paste the above code into a new scripts page in mySQL Workbench,
+-- (2) save that page with the name flightreservation,
+-- (3) run the code in mySQL Workbench to create the database,
+-- (4) leave mySQL Workbench open and running while you are running the program in IntelliJ,
