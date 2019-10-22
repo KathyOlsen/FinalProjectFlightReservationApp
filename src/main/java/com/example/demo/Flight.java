da2b33eabc30236c7fd2aa0b3263132615335cea
danielgkneto <GBTC440016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-22 10:21:28 -0400

    Finished the Flight class

--- src/main/java/com/example/demo/Flight.java
+++ src/main/java/com/example/demo/Flight.java
@@ -25,11 +25,66 @@ public class Flight {
     @Column(name = "arrival_airport_id", nullable = false)
     private long arrivalAirportId;
 
-/*
-departdatetime
-arrivaldatetime
-duration
-baseprice
- */
+    @Column(name = "duration_hours")
+    private int durationHours;
+
+    @Column(name = "baseprice")
+    private double baseprice;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getDepartureAirportId() {
+        return departureAirportId;
+    }
+
+    public void setDepartureAirportId(long departureAirportId) {
+        this.departureAirportId = departureAirportId;
+    }
+
+    public long getArrivalAirportId() {
+        return arrivalAirportId;
+    }
+
+    public void setArrivalAirportId(long arrivalAirportId) {
+        this.arrivalAirportId = arrivalAirportId;
+    }
+
+    public int getDurationHours() {
+        return durationHours;
+    }
+
+    public void setDurationHours(int durationHours) {
+        this.durationHours = durationHours;
+    }
+
+    public double getBaseprice() {
+        return baseprice;
+    }
+
+    public void setBaseprice(double baseprice) {
+        this.baseprice = baseprice;
+    }
 }
 
